7943aa0d628400a6fb2ac27dc4eb1b178fe556f0
Braian de Liz da Silva <delizbraian@gmail.com>
2025-11-25 11:16:58 -0300

    schema_json

--- src/routes/routs_user/user_cadastro.ts
+++ src/routes/routs_user/user_cadastro.ts
@@ -5,8 +5,14 @@ import { connectar } from "../../data/conecction.js";
 
 
 const cadastarr_user:FastifyPluginAsync = async (aplicacao, options) =>{
+
+    const schema_json = schema:{
+        body:{
+            
+        }
+    }
     
-    aplicacao.post("/usuario", async (request, replay) =>{
+    aplicacao.post("/usuario",schema_json ,async (request, replay) =>{
         
     });
     
