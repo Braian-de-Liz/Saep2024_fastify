dd36fc5eab965a1d17e68cf5ec346ce62800e53d
Braian-de-Liz <delizbraian@gmail.com>
2025-12-24 17:46:26 -0300

    API concluída

--- src/routes/routs_tasks/atualizarTask.ts
+++ src/routes/routs_tasks/atualizarTask.ts
@@ -5,7 +5,8 @@ import { z, ZodObject } from 'zod';
 
 const schemaPatchZod: ZodObject = z.object({
     id_tarefa: z.number().int().min(1),
-    status: z.enum(['a fazer', 'fazendo', 'pronto'])
+    status_tarefa: z.enum(['a fazer', 'fazendo', 'pronto']),
+    id_usuario: z.number().int().min(1)
 });
 
 
@@ -17,7 +18,7 @@ const AtualizarTask: FastifyPluginAsyncZod = async (aplicacao, options) => {
         try {
             const procurarTask = await aplicacao.pg.query("SELECT FROM tarefas WHERE id = $1 AND id_usuario = $2", [id_tarefa, id_usuario]);
 
-            if (!procurarTask) {
+            if (procurarTask.rowCount === 0) {
                 aplicacao.log.warn("tarefa não encontrada");
 
                 return reply.status(404).send({
@@ -25,12 +26,32 @@ const AtualizarTask: FastifyPluginAsyncZod = async (aplicacao, options) => {
                     menssagem: 'tarefa não encontrada'
                 });
             }
+            const querySql: string = `UPDATE tarefas SET status = $1 WHERE id = $2 AND id_usuario = $3`;
+
+            const Atualizar = await aplicacao.pg.query(querySql, [status_tarefa, id_tarefa, id_usuario]);
 
 
-            
+            if (Atualizar.rowCount === 0) {
+                console.error("não foi possível atualizar tarefa");
+
+                return reply.status(401).send({
+                    status: 'erro',
+                    menssagem: 'erro ao atualizar tarefa'
+                });
+            }
+
+            return reply.status(200).send({
+                status: 'tudo certo',
+                menssagem: 'atualizada com sucesso'
+            });
         }
         catch (erro) {
+            aplicacao.log.error(erro);
 
+            return reply.status(500).send({
+                status: 'erro',
+                message: 'Erro interno ao atualizar tarefa.'
+            });
         }
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,9 @@ import db from "./data/conecction.js";
 import insertTask from './routes/routs_tasks/salvar_Task.js';
 import cadastarr_user from './routes/routs_user/user_cadastro.js';
 import user_login from './routes/routs_user/user_login.js';
+import DeletarTask from "./routes/routs_tasks/deletarTask.js";
+import AtualizarTask from "./routes/routs_tasks/atualizarTask.js";
+import ListagemTasks from "./routes/routs_tasks/ListarTask.js";
 
 
 if (!process.env.JWT_PASSOWORD) {
@@ -31,6 +34,9 @@ try {
     await aplicacao.register(insertTask, { prefix: '/api' });
     await aplicacao.register(cadastarr_user, { prefix: '/api' });
     await aplicacao.register(user_login, { prefix: '/api' });
+    await aplicacao.register(DeletarTask, { prefix: '/api' });
+    await aplicacao.register(AtualizarTask, { prefix: '/api' });
+    await aplicacao.register(ListagemTasks, { prefix: '/api' });
 
     aplicacao.listen({ port: 3220, host: '0.0.0.0' }, () => {
         console.log("Fastify rodando na porta 3220 com Zod ativado!");
